4a32a8c96f6026ba9685c22246b5fa158e73aa77
AndriiAV <andrushchenko.andrii@gmail.com>
2017-05-08 19:31:58 +0300

    Pattern for phone number

--- src/site/Profile.java
+++ src/site/Profile.java
@@ -1,5 +1,8 @@
 package site;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public abstract class Profile {
     private String name;
     private String phoneNumber;
@@ -21,5 +24,24 @@ public abstract class Profile {
 
     public String getEmail() {
         return email;
+    }
+
+    public static boolean testPhoneNumber (String phoneNumber){
+        Pattern p = Pattern.compile("^\\+?+[0-9_()-]{5,20}$");
+        /*
+        ^ - початок рядка, який перевіряється
+        $ - кінець рядка, який перевіряється
+        \\+? - екранований знак "+" є необов'язковим
+        0-9_()- - набір доступних символів
+        {5,20} - може бути від 5 до 20 символів
+        */
+        Matcher m = p.matcher(phoneNumber);
+        return m.matches();
+    }
+
+    public static boolean testEmail (String email){
+        Pattern p = Pattern.compile(".+\\@.+\\..+");
+        Matcher m = p.matcher(email);
+        return m.matches();
     }
 }
