c4e7017c56fe64e0d2906f29b5f4a303336ed3be
AndriiAV <andrushchenko.andrii@gmail.com>
2017-05-08 20:25:15 +0300

    Pattern for date

--- src/site/Poster.java
+++ src/site/Poster.java
@@ -2,14 +2,11 @@ package site;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Poster {
     private final Course course;
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
     private final Date startDate;
     private final Teacher teacher;
 
@@ -18,7 +15,22 @@ public class Poster {
         this.startDate = startDate;
         this.teacher = teacher;
     }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
     SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+    public String getSdf(){
+        return sdf.format(startDate);
+    }
+
+    public static boolean testDate (String sdf){
+        Pattern p = Pattern.compile("([0-2]\\d|3[01])+\\.(0\\d|1[012])\\.(\\d{4})");
+        Matcher m = p.matcher(sdf);
+        return m.matches();
+    }
 
     @Override
     public String toString() {
