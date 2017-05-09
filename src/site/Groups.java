4d9d44fdca6ede920bd2e2ec85b7cd1fc080df34
AndriiAV <andrushchenko.andrii@gmail.com>
2017-05-09 13:41:50 +0300

    Added sortingByCourse and sortingByTeacher

--- src/site/Groups.java
+++ src/site/Groups.java
@@ -1,11 +1,30 @@
 package site;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Groups {
     private ArrayList<Group> listOfGroups = new ArrayList<>();
 
-    public void sorting (){
+    public void sortingByTeacher (){
+        Collections.sort(listOfGroups, new Comparator<Group>() {
+                    @Override
+                    public int compare(Group o1, Group o2) {
+                        return  o1.getTeacher().compareTo(o2.getTeacher());
+                    }
+                }
+        );
+    }
+
+    public void sortingByCourse (){
+        Collections.sort(listOfGroups, new Comparator<Group>() {
+                    @Override
+                    public int compare(Group o1, Group o2) {
+                        return  o1.getCourse().compareTo(o2.getCourse());
+                    }
+                }
+        );
     }
 
     public void filtering (){
