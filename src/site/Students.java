17b16e7009aced74dbb21d9ad75b818f059a10df
AndriiAV <andrushchenko.andrii@gmail.com>
2017-05-09 13:48:29 +0300

    Added sortingByName

--- src/site/Students.java
+++ src/site/Students.java
@@ -1,11 +1,20 @@
 package site;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Students {
     private ArrayList<Student> listOfStudent = new ArrayList<>();
 
-    public void sorting (){
+    public void sortingByName (){
+        Collections.sort(listOfStudent, new Comparator<Student>() {
+                    @Override
+                    public int compare(Student o1, Student o2) {
+                        return  o1.getName().compareTo(o2.getName());
+                    }
+                }
+        );
     }
 
     public void filtering (){
